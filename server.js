83a060e6b2af24ea6ede3ae900ff7fcd4624888e
Scott Feichter <scottfeichter@gmail.com>
2024-05-21 13:27:41 -0700

    done-ish

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-// Your code here 
+// Your code here
+const http = require("http");
+
+const responseBody = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <h1>Hello there!</h1>
+  </body>
+  </html>
+`;
+
+const server = http.createServer((req, res) => {
+  console.log("REQ: ", req);
+  console.log("RES: ", res);
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/css");
+  res.end(responseBody);
+});
+
+const port = 5001;
+
+server.listen(port, () => console.log("Server is listening on port", port));
